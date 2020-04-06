83ee3626798f0fb81eed7240469f1ae40436483f
Ridla <ridla@Users-MacBook-Pro.local>
2020-04-06 14:09:42 +0700

    ini adalah reinstall modoles in node dan installlation about turbo

--- middleware_body-parser/app.js
+++ middleware_body-parser/app.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const fs = require('fs')
+const path = require('path')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/add-product', (req, res,next ) => 
+    // res.send(fs.readFileSync(path.join(__dirname,'views','index.html')))
+    fs.createReadStream(path.join(__dirname,'views','add-product.html')).pipe(res)
+)
+
+app.use('/save-product', (req, res,next ) =>{
+    console.log('save product....'),
+    console.log(req.body)
+    res.send('Save succesfully')
+    }
+)
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
